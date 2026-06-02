fbcf447dac28e42eb8e89083fd4f45b198b5d9a9
Mokshsheokand11 <mokshsheokand11@gmail.com>
2026-06-03 04:55:24 +0530

    api put

--- Bluestock_B100_Intelligence/backend/bluestock/urls.py
+++ Bluestock_B100_Intelligence/backend/bluestock/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+path('api/', include('backend.api.urls')),
     path('', include('api.urls')),   # This makes frontend load at root
 ]
